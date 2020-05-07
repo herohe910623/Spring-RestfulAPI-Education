eabf08e6d12d87010359fd787923adbedb2e687e
Your Name <herohe910623@gmail.com>
2020-05-07 15:14:48 +0900

    requestparam

--- src/main/java/kr/re/kitri/hello/controller/HomeController.java
+++ src/main/java/kr/re/kitri/hello/controller/HomeController.java
@@ -4,10 +4,7 @@ import kr.re.kitri.hello.service.TeamService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.awt.*;
 import java.util.jar.Attributes;
@@ -31,7 +28,10 @@ public class HomeController {
     @GetMapping(value="/teams",
             produces = {MediaType.APPLICATION_XML_VALUE, MediaType.APPLICATION_JSON_VALUE}
     )//팀 목록보기
-    public String getAllTeams() {
+    public String getAllTeams(@RequestParam(name = "page", defaultValue = "1") int page,
+                              @RequestParam(name = "limit", defaultValue = "5") int limit) {
+        System.out.println("page number = " + page);
+        System.out.println("limit = " + limit);
         //파라미터나 인증 정보 이런것들이 있으면 이걸 처리해주는 역할
         //기능을 수행.. (전체 팀 조회).. 비지니스레이어로 위임
         //전체팀 조회한 데이터를 받는다. from 비지니스 레이어에서
